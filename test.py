65cc5ffb2ac7f635c8592039fd0ba87f81b3f237
Martin H <martin.hippenstiel@gmail.com>
2022-11-27 05:35:54 +0100

    IBAN und Gesamtbetrag hinzugefügt; Daten werden in einer Liste aus Dicts abgespeichert.

--- test.py
+++ test.py
@@ -19,8 +19,10 @@ def file_list():
 
 
 def pdf_text_extraction():
+    all_datasets = list() # Hier werden die extrahierten Datensätze gesammelt
     """Extrahiert den Text aller PDF Dateien mit dem Dateinamen aus der Liste "files" mittels for Schleife"""
     for item in files:
+        current_dataset = dict()
         working_directory = os.getcwd()
         file_path = working_directory + "\\" + folder + "\\" + item
         print(file_path)
@@ -29,6 +31,7 @@ def pdf_text_extraction():
         x=pdfreader.numPages
         pageobj=pdfreader.getPage(x-1)
         text=pageobj.extractText() #hier können die regex angesetzt werd, wir erhalten hier Strings
+        #print(text)
 
         # Bitte hier unten die Regex einsetzen (wird später als eigene Funktion definiert)
         # mit Print testen
@@ -37,15 +40,36 @@ def pdf_text_extraction():
         firmenname = re.findall("[A-z0-9]+@([A-z0-9]+).",text)
         if firmenname:
             firmenname = firmenname[0]      #Firmenname final  (von Email Adresse extrahiert, Achtung aktuell enthält nur eine Rechnung eine Mailadresse, deswegen sind einige Listen leer)
+            current_dataset["firmenname"] = firmenname
             print(firmenname.title())
 
         #Regex: Datum
         datum = re.findall("([0-9]{2}\.[0-9]{2}\.[0-9]{2,4})",text)
         datum = min(datum)                    #Rechnungsdatum final  (vorerst kleinstes Datum aus Rechnung gewählt)
+        current_dataset["Datum"] = datum
         print(datum)
 
+        # Regex: IBAN
+        # https://de.wikipedia.org/wiki/Internationale_Bankkontonummer#Zusammensetzung
+        # Annahme: Kontoidentifikation 11..30 Ziffern (theoretisch auch Buchstaben, aber dann wirds schwierig)
+        iban = re.findall(r"[a-zA-Z]{2}\d{2}\s?(?:\d\s?){11,30}", text)
+        if iban:
+            iban = re.sub(r"\s+","",iban[0])   
+            current_dataset["iban"] = iban
+            print(iban)
+        
+        # Regex: Gesamtbetrag
+        # Annahme: Komma als Dezimaltrennzeichen
+        gesamtbetrag = re.findall(r"\d{1,3}(?:\s?\d\d\d)*,\d\d", text)
+        gesamtbetrag = [float(i.replace(",",".").replace(" ","")) for i in gesamtbetrag]
+        gesamtbetrag = max(gesamtbetrag)
+        current_dataset["gesamtbetrag"] = gesamtbetrag
+        print(gesamtbetrag)
+        
+        all_datasets.append(current_dataset)
+    return all_datasets
 
 #führt aus
 file_list()
-pdf_text_extraction()
+print(pdf_text_extraction())
 
